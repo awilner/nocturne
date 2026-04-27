afce9414eb04a215bcfe0c7f47ccededb2ab376a
Rhys <36913739+ryceg@users.noreply.github.com>
2026-04-27 12:55:19 +1000

    feat: add actogram barrel export

---
+++ src/Web/packages/app/src/lib/components/actogram/index.ts
@@ -0,0 +1,9 @@
+export { default as Actogram } from './Actogram.svelte';
+export { default as ActogramRow } from './ActogramRow.svelte';
+export type {
+	ActogramPoint,
+	ActogramRowContext,
+	GlucosePoint,
+	GlucoseThresholds,
+	RowDataPoint,
+} from './actogram';
